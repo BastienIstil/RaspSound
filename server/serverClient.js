ece1ab29a02095aaba4744ee0c298c9643502687
root <root@meteor>
2016-07-20 16:34:19 +0200

    Ajout de la récupèration des id de sockets

--- server/serverClient.js
+++ server/serverClient.js
@@ -3,7 +3,7 @@ function ServerClient() {
   ServerClient.app = require('express')();
   ServerClient.http = require('http').Server(ServerClient.app);
   ServerClient.io = require('socket.io')(ServerClient.http);
-  ServerClient.clientEvent = require('events');
+  ServerClient.activeSocket = [];
 }
 
 // class methods
@@ -14,11 +14,19 @@ ServerClient.prototype.initServer = function() {
     });
 
     ServerClient.io.on('connection', function(socket){
-      console.log('connection from client');
+      console.log('connection from client' + socket.id);
+
+      ServerClient.io.to(socket.id).emit('chat message', socket.id);
+
+      ServerClient.activeSocket.push(socket.id);
+
       socket.on('chat message', function(msg){
-        console.log('message  from client! ');
-        console.log(msg);
+        console.log('message  from ' + socket.id + ' ' + msg);
         ServerClient.io.emit('chat message', msg);
+      });
+
+      socket.on('disconnect', function(){
+        console.log('disconnect from ' + this.id);
       });
     });
 
